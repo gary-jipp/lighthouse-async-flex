d735df8e68b6222d59fe5371d9907908a7cf9f38
gary <github@accu.ca>
2024-02-10 12:51:18 -0800

    Update

--- 02-timeout.js
+++ 02-timeout.js
@@ -1,15 +1,23 @@
 
 console.log("01: Start of the code");
 
+const doStuff = function() {
+  console.log("Doing stuff now");
+};
+
+setTimeout(doStuff, 3000);
+
+// If we set the timeout to 1 ms.??
 setTimeout(() => {
-  console.log("02: We are inside the callback");
-}, 30);
+  console.log("02: We are inside the 1 ms callback");
+}, 1);
 
 
 setTimeout(() => {
-  console.log("03: We are inside the callback");
+  console.log("03: We are inside the 20 ms callback");
 }, 20);
 
+// Will this be interrupted by our setTimeout ?
 for (let i = 0; i < 100; i++) {
   const date = new Date();
   console.log(date, i);
